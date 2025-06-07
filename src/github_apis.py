2fa3c3cdb70cc6a68b966d40d60e264236c7e611
AbhilakshSinghReen <abhilakshreen@gmail.com>
2025-06-07 16:28:55 +0530

    Added getting repo meta

--- src/github_apis.py
+++ src/github_apis.py
@@ -1,6 +1,14 @@
-def get_repo_star_count(repo_url):
-    pass
+import requests
 
 
-def get_repo_issue_count(repo_url):
-    pass
+requests_session = requests.Session()
+
+
+def get_repo_metadata(repo_endpoint):
+    url = "https://api.github.com/repos/" + repo_endpoint
+    
+    response = requests_session.get(url)
+    
+    response_data = response.json()
+    
+    return response_data
--- src/main.py
+++ src/main.py
@@ -1,12 +1,23 @@
-from src.github_apis import get_repo_star_count, get_repo_issue_count
+from src.github_apis import get_repo_metadata
 
 
-repo_url = "https://github.com/kubernetes/kubernetes"
+repo_url = "kubernetes/kubernetes"
 
 
 def main():
-    print(get_repo_star_count(repo_url))
-    print(get_repo_issue_count(repo_url))
+    repo_metadata = get_repo_metadata(repo_url)
+    
+    stars = repo_metadata.get('stargazers_count', "N/A")
+    open_issues = repo_metadata.get('open_issues_count', "N/A")
+
+    languages_url = repo_metadata.get('languages_url', None)
+
+    print(stars)
+    print(open_issues)
+    print(languages_url)
+
+    # print()
+    # print(get_repo_issue_count(repo_url))
 
 
 if __name__ == "__main__":
